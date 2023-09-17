da9614c9355756aaa1bc05a66fcbccd4658cfac4
wuxh <wxh1220@gmail.com>
2023-09-17 22:22:11 +0800

    chore: update case

--- tests/snippets.spec.ts
+++ tests/snippets.spec.ts
@@ -32,117 +32,38 @@ describe('snippet', () => {
   });
 
   it('带高亮行', () => {
-    expect(remark().use(core, {}).processSync(generateFile(`<<< ./say-hi.ts{1}`)).toString())
-      .toMatchInlineSnapshot(`
-      "\`\`\`ts {1}| pure
-      console.log('Hello dumi-plugin-code-snippets!');
-
-      export function doSomething() {
-        return 1;
-      }
-
-      \`\`\`
-      "
-    `);
+    expect(
+      remark().use(core, {}).processSync(generateFile(`<<< ./say-hi.ts{1}`)).toString(),
+    ).toMatchSnapshot();
   });
 
   it('支持tsx', () => {
-    expect(remark().use(core, {}).processSync(generateFile(`<<< ./My.tsx`)).toString())
-      .toMatchInlineSnapshot(`
-      "\`\`\`tsx | pure
-      import * as React from 'react';
-
-      export interface MyProps {
-        slogan?: React.ReactNode;
-      }
-
-      // #region snippet
-      function My(props: React.PropsWithChildren<MyProps>) {
-        const { children } = props;
-        return (
-          <>
-            <div className=\\"my-slogan\\">
-              <p>魔法师正在进行最后的仪式，为您带来一项惊艳功能</p>
-              <strong>TBD: The Brilliant Discovery!</strong>
-            </div>
-            {children}
-            {/* This is my.tsx} */}
-          </>
-        );
-      }
-      // #endregion snippet
-
-      export default My;
-
-      \`\`\`
-      "
-    `);
+    expect(
+      remark().use(core, {}).processSync(generateFile(`<<< ./My.tsx`)).toString(),
+    ).toMatchSnapshot();
   });
 
   it('支持 vscode region', () => {
-    expect(remark().use(core, {}).processSync(generateFile(`<<< ./My.tsx#snippet`)).toString())
-      .toMatchInlineSnapshot(`
-      "\`\`\`tsx | pure
-      function My(props: React.PropsWithChildren<MyProps>) {
-        const { children } = props;
-        return (
-          <>
-            <div className=\\"my-slogan\\">
-              <p>魔法师正在进行最后的仪式，为您带来一项惊艳功能</p>
-              <strong>TBD: The Brilliant Discovery!</strong>
-            </div>
-            {children}
-            {/* This is my.tsx} */}
-          </>
-        );
-      }
-      \`\`\`
-      "
-    `);
+    expect(
+      remark().use(core, {}).processSync(generateFile(`<<< ./My.tsx#snippet`)).toString(),
+    ).toMatchSnapshot();
   });
 
   it('支持 region + 高亮行', () => {
-    expect(remark().use(core, {}).processSync(generateFile(`<<< ./My.tsx#snippet{4}`)).toString())
-      .toMatchInlineSnapshot(`
-      "\`\`\`tsx {4}| pure
-      function My(props: React.PropsWithChildren<MyProps>) {
-        const { children } = props;
-        return (
-          <>
-            <div className=\\"my-slogan\\">
-              <p>魔法师正在进行最后的仪式，为您带来一项惊艳功能</p>
-              <strong>TBD: The Brilliant Discovery!</strong>
-            </div>
-            {children}
-            {/* This is my.tsx} */}
-          </>
-        );
-      }
-      \`\`\`
-      "
-    `);
+    expect(
+      remark().use(core, {}).processSync(generateFile(`<<< ./My.tsx#snippet{4}`)).toString(),
+    ).toMatchSnapshot();
   });
 
   it('支持 css 文件', () => {
-    expect(remark().use(core, {}).processSync(generateFile(`<<< ./style.css`)).toString())
-      .toMatchInlineSnapshot(`
-      "\`\`\`css | pure
-      #App {
-        color: red;
-      }
-
-      \`\`\`
-      "
-    `);
+    expect(
+      remark().use(core, {}).processSync(generateFile(`<<< ./style.css`)).toString(),
+    ).toMatchSnapshot();
   });
 
   it.skip('路径错误返回错误信息', () => {
-    expect(remark().use(core, {}).processSync(generateFile(`<<< ./aa/bb/style.css`)).toString())
-      .toMatchInlineSnapshot(`
-      "\`\`\`css | pure
-      Code snippet path not found: /Users/wuxh/Code/SelfProject/dumi-plugin-code-snippets/tests/__fixtures__/aa/bb/style.css
-      \`\`\`
-      "
-    `);
+    expect(
+      remark().use(core, {}).processSync(generateFile(`<<< ./aa/bb/style.css`)).toString(),
+    ).toMatchSnapshot();
   });
 });
